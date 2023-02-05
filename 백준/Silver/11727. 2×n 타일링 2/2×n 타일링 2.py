d21d3c856927f8f0c7b7bbf24bddd395f2c85148
Jongmun Han <56660640+Dakota-Han@users.noreply.github.com>
2023-02-05 12:30:32 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,16 @@
+import sys
+read = sys.stdin.readline
+
+n = int(read().rstrip())
+
+dp=[0]*1010
+
+dp[1]=1
+dp[2]=3
+dp[3]=5
+count = 1
+if(n>3):
+    for i in range(4,n+1):
+        dp[i] = dp[i-1] + 2*dp[i-2]
+
+print(dp[n]%10007)
